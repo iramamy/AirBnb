64f9dc461dbb50843afa46e534319e3aa76ee248
iramamy <mamy@aims.ac.za>
2024-08-27 10:59:58 +0200

    dynamic inbox list from db

--- app/components/inbox/Conversation.tsx
+++ app/components/inbox/Conversation.tsx
@@ -1,10 +1,30 @@
-import React from "react";
+"use client";
 
-const Conversation = () => {
+import { useRouter } from "next/navigation";
+
+// Custom components
+import { ConversationType } from "@/app/inbox/page";
+
+interface ConversationProps {
+  conversation: ConversationType;
+  userId: string;
+}
+const Conversation: React.FC<ConversationProps> = ({
+  conversation,
+  userId,
+}) => {
+  const router = useRouter();
+  const otherUser = conversation.users.find((user) => user.id != userId);
+
   return (
     <div className="px-6 py-4 border border-gray-300 rounded-xl cursor-pointer">
-      <p className="mb-6 text-xl hover:text-gray-700">John Doe</p>
-      <p className="text-airbnb-dark hover:text-airbnb">Go to conversation</p>
+      <p className="mb-6 text-xl hover:text-gray-700">{otherUser?.name}</p>
+      <p
+        onClick={() => router.push(`/inbox/${conversation.id}`)}
+        className="text-airbnb-dark hover:text-airbnb"
+      >
+        Go to conversation
+      </p>
     </div>
   );
 };
